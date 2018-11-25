0eef996090b805d1a211d42a40a3ae3369c25b2c
imeace <songyingxinok@gmail.com>
2018-11-25 23:46:42 +0800

    更换卡片Lib

--- miniprogram/pages/slideCard/slideCard.js
+++ miniprogram/pages/slideCard/slideCard.js
@@ -1,234 +1,371 @@
-// pages/slideCard/slideCard.js
+//index.js
+//获取应用实例
+var app = getApp();
+
+var allCount=0;
+
+
+
+
+//ALL:假数据,若加载到服务器数据,可将ALL掷为[],在onLoad函数中请求服务器数据赋给ALL;(ps:记得在所有数据后面附上两条假数据,如下ALL中倒数这两条数据,因为这两条数据用于处理层叠效果,若改变其id,在对应的函数处理判断位置应该修改其判断值)
+//bug:ALL数据应为奇数条,不然会导致最后层叠效果无法消失
+//animationData(动画),want_hidden(点击要,将要隐藏起来),nowant_hidden 初始化对应的数据
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    animationData1: {},
-    animationData2: {},
-    topPos1: 120,
-    leftPos1: 42,
-    topPos2: 120,
-    leftPos2: 42,
-    isShow: true,
-    startX: 0,
-    startY: 0,
-    windowWidth:0
+    animationData: {},
+    want_hidden:false,
+    nowant_hidden:true,
+    All: [{
+      cardUrl: '../../images/test1.jpg',
+      id:1
+     
+    }, {
+        cardUrl: '../../images/test2.jpg',
+      id:2
+
+    
+    }, {
+        cardUrl: '../../images/test3.jpg',
+      id:3
+    },
+    
+    {
+        cardUrl: '../../images/test3.jpg',
+      id:4
+    },
+    
+    {
+        cardUrl: '../../images/test4.jpg',
+      id:5   
+    },
+    
+    {
+        cardUrl: '../../images/test5.jpg',
+      id:6
+    },
+    
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that=this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          windowWidth:res.windowWidth
-        })
-      }
+
+
+
+  // 滑动第一张的移动事件
+  //pageX,pageY,当前移动点位置
+  //moveX,moveY用于锁定图片中点位置
+  //ballLeft由于是rpx所以*2
+
+  touchmove:function(event) {
+    console.log(event)
+    let pageX = event.touches[0].pageX;
+    let pageY = event.touches[0].pageY;
+    // 需要移动的距离;
+    let moveX=this.data.screenWidth*0.8*0.5;
+    let moveY=350*0.5;
+          this.setData ({
+            ballTop: event.touches[0].pageY - moveY,
+            ballLeft: (event.touches[0].pageX - moveX)*2,
+          });   
+     
+  },
+
+
+// 第一张移动结束处理动画
+  touchend:function(event){
+        if(event.currentTarget.offsetLeft<-110){
+          this.Animation(-500);
+        }else if(event.currentTarget.offsetLeft>180){
+          this.Animation(500);
+        }else{
+          this.AnimationN1(event.currentTarget.offsetLeft,event.currentTarget
+    .offsetTop);
+        }
+
+  },
+
+
+
+//第二张移动事件  
+//pageX,pageY,当前移动点位置
+   touchmove2:function(event) {
+    let pageX = event.touches[0].pageX;
+    let pageY = event.touches[0].pageY;
+    console.log(pageX,pageY);
+    // 需要移动的距离;
+    let moveX=this.data.screenWidth*0.8*0.5;
+    let moveY=350*0.5;
+    this.setData ({
+      ballTop2: event.touches[0].pageY - moveY,
+      ballLeft2: (event.touches[0].pageX - moveX)*2,
     });
-    console.log(options)
+    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
+// 第二张移动结束处理动画
+touchend2:function(event){
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    if(event.currentTarget
+.offsetLeft<-110){
+     
+      this.Animation2(-500);
 
-  },
-
-  touchS1: function (event) {
-    var pointXData = event.touches[0].clientX;
-    var pointYData = event.touches[0].clientY;
-    this.setData({
-      startX: pointXData,
-      startY: pointYData
-    })
-  },
-  touchM1: function (event) {
-    var pointXData = event.touches[0].clientX;
-    var pointYData = event.touches[0].clientY;
-    var widthMiddle=this.data.windowWidth/2
-
-    var perX = Math.abs(pointXData - widthMiddle) / widthMiddle
-    var perY = Math.abs(pointYData - 575) / 575
-
-
-    if (perY > perX) {
-      this.showBottom2(perY)
-    } else {
-      this.showBottom2(perX)
+    }else if(event.currentTarget
+.offsetLeft>180){
+             this.Animation2(500);    
+    }else{
+      this.AnimationN2(event.currentTarget
+.offsetLeft,event.currentTarget
+.offsetTop);
     }
+      
+},
 
-
-
-    var XData = pointXData * 2 - 332;
-    var YData = pointYData * 2 - 475;
-
-    this.setData({
-      leftPos1: XData,
-      topPos1: YData
-    })
-  },
-  touchE1: function (event) {
-    var endXData = event.changedTouches[0].clientX;
-    var endYData = event.changedTouches[0].clientY;
-    var disX = endXData - this.data.startX;
-    var disY = endYData - this.data.startY
-    console.log(disX)
-
-
-    if (disX > 93.75) {
-      this.moveXBox('right')
-    }
-    if (disX < -93.75) {
-      this.moveXBox('left')
-    }
-    if (disY < -150) {
-      this.moveXBox('up')
-    }
-    if (disY > 150) {
-      this.moveXBox('down')
-    }
-
-    if (disX < 93.75 && disX > -93.75 && disY > -150 && disY < 150) {
-      this.replace1()
-    }
-
-
-
-  },
-  moveXBox: function (direction) {
+// 第一张左滑动右滑动动画
+Animation:function(translateXX){
+ 
+       // 动画
     var animation = wx.createAnimation({
-      duration: 250,
-      timingFunction: 'linear',
-    })
-    if (direction == 'right') {
-      animation.translateX(400).rotate(45).opacity(0).step();
-      animation.translateX(0).rotate(0).step();
-    }
-    if (direction == 'left') {
-      animation.translateX(-400).rotate(-45).opacity(0).step();
-      animation.translateX(0).rotate(0).step();
-    }
-    if (direction == 'up') {
-      animation.translateY(-400).opacity(0).step();
-      animation.translateY(0).step();
-    }
-    if (direction == 'down') {
-      animation.translateY(400).opacity(0).step();
-      animation.translateY(0).step();
-    }
+      duration: 720,
+      // timingFunction: 'cubic-bezier(.8,.2,.1,0.8)',
+        timingFunction: "ease",
 
+    });
+    this.animation= animation;
+    this.animation.translateY(0).translateX(translateXX).step();
+    this.animation.translateY(0).translateX(0).opacity(1).rotate(0).step({duration: 10});
+
+
+    
     this.setData({
-      animationData1: animation.export(),
-      isShow: false
-    })
-    setTimeout(function () {
-      this.setData({
-        topPos1: 120,
-        leftPos1: 42
-      })
-    }.bind(this), 500)
-  },
-  replace1: function () {
-    this.setData({
-      topPos1: 120,
-      leftPos1: 42
-    })
-  },
-  showBottom1: function (per) {
+      animationData: this.animation.export(),
+
+    
+
+    });
+    var self=this;
+    setTimeout(function(){
+        self.setData({
+        ballTop:62,
+        ballLeft:76,
+        index1:4,
+        percent2:100,
+        index2:6,
+        })
+    },720);
+        setTimeout(function() {
+      var cardInfoList = self.data.cardInfoList;
+      if(self.data.All.length>0){
+        var tempfromAll=self.data.All.shift();
+        self.data.cardInfoList[0]=tempfromAll;
+        }
+  
+                  
+  //当加载最后一条数据时划出后隐藏自己
+            if(self.data.cardInfoList[0].id==allCount){
+             
+          //   self.setData({
+          //       hidden1:true,
+              
+          // })
+               wx.showToast({
+                        title: '已无更多',
+                        icon: 'success',
+                        duration: 2000
+                      })   
+          
+        }
+
+        self.setData({
+        cardInfoList: self.data.cardInfoList,
+        animationData: {},
+      });
+    }, 350);
+
+},
+// 第二张左滑动右滑动动画
+Animation2:function(translateXX){
+       // 动画
     var animation = wx.createAnimation({
-      duration: 100,
+      duration: 720,
+      // timingFunction: 'cubic-bezier(.8,.2,.1,0.8)',
+        timingFunction: "ease",
+
+    });
+   this.animation= animation;
+    this.animation.translateY(0).translateX(translateXX).opacity(0,1).step();
+    this.animation.translateY(0).translateX(0).opacity(1).rotate(0).step({duration: 10});
+
+
+    
+    this.setData({
+      animationData1: this.animation.export(),
+    });
+
+
+    var self=this;
+    setTimeout(function(){
+        self.setData({
+        percent1:100,
+        index1:6, 
+
+        ballTop2:62,
+        ballLeft2:76,
+        index2:4,
+    
+        })
+    },720)
+
+
+          setTimeout(function() {
+      var cardInfoList = self.data.cardInfoList;
+   
+      if(self.data.All.length>0){
+
+        var tempfromAll=self.data.All.shift();
+        self.data.cardInfoList[1]=tempfromAll;
+
+
+      } 
+  
+
+        self.setData({
+        cardInfoList: self.data.cardInfoList,
+        animationData: {},
+
+      });
+    }, 350);
+
+},
+
+
+    // 第一张图片不需翻页动画
+AnimationN1:function(offsetX,offsetY){
+   // 动画
+    var animation = wx.createAnimation({
+      duration: 500,
       timingFunction: 'ease-out',
-    })
-    animation.opacity(per).step();
+        // timingFunction: "ease",
+
+    });
+    var self = this;
+    this.animation= animation;
+   this.animation.translateX(offsetX).translateY(offsetY).rotate(0).step({duration: 10});
+    this.animation.translateY(0).translateX(0).rotate(0).scale(1).step();
+    
     this.setData({
-      animationData1: animation.export()
-    })
-  },
+        animationData: this.animation.export(),
+        ballTop:62,
+        ballLeft:76,
+    });
+},
 
 
-
-  touchS2: function (event) {
-    var pointXData = event.touches[0].clientX;
-    var pointYData = event.touches[0].clientY;
-    this.setData({
-      startX: pointXData,
-      startY: pointYData
-    })
-  },
-  touchM2: function (event) {
-    var pointXData = event.touches[0].clientX;
-    var pointYData = event.touches[0].clientY;
-    var widthMiddle = this.data.windowWidth / 2
-
-    var perX = Math.abs(pointXData - widthMiddle) / widthMiddle
-    var perY = Math.abs(pointYData - 575) / 575
-
-    if (perY > perX) {
-      this.showBottom1(perY)
-    } else {
-      this.showBottom1(perY)
-    }
-
-    var XData = pointXData * 2 - 332;
-    var YData = pointYData * 2 - 475;
-    this.setData({
-      leftPos2: XData,
-      topPos2: YData
-    })
-  },
-  touchE2: function (event) {
-    var endXData = event.changedTouches[0].clientX;
-    var endYData = event.changedTouches[0].clientY;
-
-    var disX = endXData - this.data.startX;
-    var disY = endYData - this.data.startY
-    console.log(disX)
-
-
-    if (disX > 93.75) {
-      this.moveXBox('right')
-    }
-    if (disX < -93.75) {
-      this.moveXBox('left')
-    }
-    if (disY < -150) {
-      this.moveXBox('up')
-    }
-    if (disY > 150) {
-      this.moveXBox('down')
-    }
-
-    if (disX < 93.75 && disX > -93.75 && disY > -150 && disY < 150) {
-      this.replace2()
-    }
-  },
-  replace2: function () {
-    this.setData({
-      topPos2: 120,
-      leftPos2: 42
-    })
-  },
-  showBottom2: function (per) {
+// 第二张图片不需翻页动画
+AnimationN2:function(offsetX,offsetY){
+   // 动画
     var animation = wx.createAnimation({
-      duration: 100,
+      duration: 500,
       timingFunction: 'ease-out',
-    })
-    animation.opacity(per).step();
-    this.setData({
-      animationData2: animation.export()
-    })
-  }
+        // timingFunction: "ease",
 
+    });
+    var self = this;
+    this.animation= animation;
+      this.animation.translateX(offsetX).translateY(offsetY).rotate(0).step({duration: 10});
+    this.animation.translateY(0).translateX(0).scale(1).rotate(0).step();
+    
+    this.setData({
+        animationData1: this.animation.export(),
+        ballTop2:62,
+        ballLeft2:76,
+    });
+},
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+// 加载数据
+//temp从ALL取出的数据
+//cardInfoList:temp;将ALL取出的数据放入以供显示
+//ballTop、ballLeft     第一张初始图片位置
+//ballTop2、ballLeft2   第二张初始图片位置
+//index1:6,index2:4,    两张图片初始的z-index
+    
+onLoad: function () {
+    var that = this;
+
+    // 请求服务器数据
+    // wx.request({
+    //   url: 'https://service.woyao.huoxingwan.com',
+    //   data: {},
+    //   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //   // header: {}, // 设置请求的 header
+    //   success: function(res){
+    //     // success
+    //     console.log(res+"222");
+    //     // that.setData({
+    //       // All:res中的数组资源
+    //     // })
+    //   },
+    //   fail: function(res) {
+    //     // fail
+    //   },
+    //   complete: function(res) {
+    //     // complete
+    //   }
+    // })
+
+
+
+
+    //调用应用实例的方法获取全局数据
+    // 创建一个临时数组 ,用于加载值(2个)
+   allCount=that.data.All.length;
+    var temp = new Array();
+    for(let i=0;i<2;i++){
+      var add=that.data.All.shift();
+      temp.push(add);
+    }
+    
+    console.log(that.data.All.length);
+    console.log(temp);
+    that.setData({
+    cardInfoList:temp,  
+
+
+    ballTop:62,
+    ballLeft:76,
+    ballTop2:62,
+    ballLeft2:76,
+    index1:6,
+    index2:4,
+        
+    })
+
+    wx.getSystemInfo({
+      success: function(res) {
+        // success
+        that.setData({
+        screenHeight: res.windowHeight,
+       screenWidth: res.windowWidth,
+        })
+        
+      }
+    })
+}
 })
+
+
+
+
