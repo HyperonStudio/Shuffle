a85d393340e27c64077e907d7fc0f94c4ad9fbdc
Noritoak <Nightonke@outlook.com>
2018-12-08 12:19:00 +0800

    上传图片功能

--- miniprogram/pages/postCard/postCard.js
+++ miniprogram/pages/postCard/postCard.js
@@ -1,8 +1,30 @@
 // miniprogram/pages/postCard/postCard.js
 
 const util = require('../../util.js');
+const COS = require('../../cos-wx-sdk-v5.js');
 
 var app = getApp();
+var config = {
+    Bucket: 'loocup-1257446936',
+    Region: 'ap-chengdu'
+};
+var taskId;
+
+// 初始化实例
+var cos = new COS({
+    getAuthorization: function(options, callback) {
+        var authorization = COS.getAuthorization({
+            SecretId: 'AKIDmRwGiNeObnrjR19fiJo7EBwXwCKG39qL',
+            SecretKey: 'Unu34tXGD5nKivGQO9jMtMdtCLQKH5k0',
+            Method: options.Method,
+            Key: options.Key,
+            Query: options.Query,
+            Headers: options.Headers,
+            Expires: 60,
+        });
+        callback(authorization);
+    }
+});
 
 Page({
 
@@ -12,16 +34,17 @@ Page({
     data: {
         inputValue: '',
         searchResult: [],
+        uploadedImageUrl: '',
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
 
     },
 
-    bindKeyInput: function (e) {
+    bindKeyInput: function(e) {
         this.setData({
             inputValue: e.detail.value
         })
@@ -58,18 +81,64 @@ Page({
                             searchResult: this.data.searchResult.concat(formattedSongList)
                         });
                         this.postCard()
-                    }
-                    else {
+                    } else {
                         this.setData({
                             searchResult: []
                         });
                     }
                 },
-                fail: function (err) {
+                fail: function(err) {
                     util.tip('搜索失败');
                 }
             });
         }
+    },
+
+    chooseImageAndUpload: function() {
+        let that = this;
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original'],
+            sourceType: ['album', 'camera'],
+            success: function(res) {
+                cos.postObject({
+                    Bucket: config.Bucket,
+                    Region: config.Region,
+                    Key: that.getImageKey(),
+                    FilePath: res.tempFilePaths[0],
+                    TaskReady: function(tid) {
+                        taskId = tid
+                    },
+                    onProgress: function(info) {
+                        console.log(JSON.stringify(info));
+                    }
+                }, function(err, data) {
+                    console.log(err || data);
+                    that.setData({
+                        uploadedImageUrl: data.Location,
+                    })
+                    if (err && err.error) {
+                        wx.showModal({
+                            title: '上传失败',
+                            content: '上传失败：' + (err.error.Message || err.error) + '；状态码：' + err.statusCode,
+                            showCancel: false
+                        });
+                    } else if (err) {
+                        wx.showModal({
+                            title: '上传出错',
+                            content: '上传出错：' + err + '；状态码：' + err.statusCode,
+                            showCancel: false
+                        });
+                    } else {
+                        wx.showToast({
+                            title: '上传成功',
+                            icon: 'success',
+                            duration: 1000
+                        });
+                    }
+                });
+            }
+        })
     },
 
     postCard: function() {
@@ -101,4 +170,17 @@ Page({
             }
         })
     },
+
+    getImageKey: function() {
+        var currentdate = new Date();
+        var datetime = "Image_" + currentdate.getFullYear() + "_" +
+            (currentdate.getMonth() + 1) + "_" +
+            currentdate.getDate() + "_" +
+            currentdate.getHours() + "_" +
+            currentdate.getMinutes() + "_" +
+            currentdate.getSeconds() + "_" +
+            currentdate.getMilliseconds() + "_" +
+            Math.floor((Math.random() * 100000) + 1);
+        return datetime;
+    }
 })
